45ccc3fef8b98f31f88f97b385872cfca3a9e98c
rs4787 <rs4787@users.noreply.github.com>
2021-09-29 19:52:18 +0000

    Python and physical interfaces

---
+++ netfacd/interface_reader.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import netifaces
+print(netifaces.interfaces())
+for i in netifaces.interfaces():
+    print('\n**************Details of Interface - ' + i + ' *********************')
+    try:    # This is a new line, you also need to indent the code below this line by 4 whitespaces
+        print('MAC: ', end='') # This print statement will always print MAC without an end of line
+        print(netifaces.ifaddresses(i)[netifaces.AF_LINK][0]['addr'])
+        print('IP: ', end='')  # This print statement will always print IP without an end of line
+        print(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+    except:          # This is a new line
+        print('Could not collect adapter information') # Print an error message
